3fa7382c021bc0973cea4d321e2d05ec83f86689
Nithin-uj <girishnithin616@gmail.com>
2024-01-02 02:03:32 +0530

    nav

---
+++ header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>GMECBH  </title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"> 
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+</head>
+<body>   
+    <div class="d-flex mx-auto">
+        <img src="amb.png" class="m-2" alt="logo" width="80px">
+        <span class="fs-1 sw-bold m-3">Govt. Medical and Engineering Boys Hostel Library </span>
+    </div>
+	<nav class="navbar navbar-expand navbar-dark bg-dark">
+	  <div class="container-fluid">	    
+		  <ul class="navbar-nav me-auto order-0">
+			<li class="nav-item">
+			  <a class="nav-link active" aria-current="page" href="#">Home</a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="#">Admin Login</a>
+			</li>
+			<li class="nav-item">
+			 <!-- <a class="nav-link" href="#">Student</a> -->
+             <div class="dropdown">
+  <button class="btn btn-dark dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Student
+  </button>
+  <ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="#">Register</a></li>
+    <li><a class="dropdown-item" href="#">Search Books</a></li>
+  </ul>
+</div>
+			</li>
+            </ul>
+
+		</div>
+	</nav>
+</body>
+</html>
+
+</html>
