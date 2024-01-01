d7c116457962ce7920eb08d78ab1fc531b08e46b
Nithin-uj <girishnithin616@gmail.com>
2024-01-02 00:52:01 +0530

    inside index

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GMECBH</title>
+</head>
+<body>
+    <h1>Welcome to GMECBH Library</h1>
+</body>
+</html>
